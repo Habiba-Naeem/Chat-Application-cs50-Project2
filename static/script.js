5c30d7579f3483888f54a5ff869e9db9542985b5
Habiba-Naeem <habiba.abdulnaeem@gmail.com>
2020-04-12 20:00:22 +0500

    sign in input working fine

--- static/script.js
+++ static/script.js
@@ -1,13 +1,16 @@
-var display_name = [];
-
 document.addEventListener('DOMContentLoaded', () =>{
+
     var okay_button = document.querySelector('#name');
     var name = document.querySelector(".name");
+    
+    
     // Connect to websocket
     //var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     
+
     // By default, submit button is disabled
     okay_button.disabled = true;
+
 
     // Enable button if there is some text in input field 
     name.onkeyup = function(e){
@@ -21,7 +24,6 @@ document.addEventListener('DOMContentLoaded', () =>{
     
                 // Now if button is clicked
                 okay_button.onclick = () => {
-                    display_name.push(name.value);
                     localStorage.setItem("name", name.value);
                     name.value = "";
                     okay_button.disabled = true;
@@ -30,9 +32,10 @@ document.addEventListener('DOMContentLoaded', () =>{
             else{
                 okay_button.disabled = true;
             }
-        }
-        
+        }   
     }
+
+
     //start by loading the index page 
 
 });
