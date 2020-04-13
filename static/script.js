ddc04588c9f5e3043a339104bfbbf525bb319206
Habiba-Naeem <habiba.abdulnaeem@gmail.com>
2020-04-13 13:20:59 +0500

    changes made

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     var okay_button = document.querySelector('#name');
     var name = document.querySelector(".name");
-    
+    var old_user_button =  document.querySelector('#old-user');
     // Connect to websocket
     //var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     
@@ -33,27 +33,24 @@ document.addEventListener('DOMContentLoaded', () =>{
                         
                         // Extract JSON data from request
                         const data = JSON.parse(request.responseText);
-
+                        console.log(data);
                         if (data.success){
                             localStorage.setItem("name", name.value);
+                            document.getElementById("display-name").style.visibility = "hidden";
+                            document.getElementById("nav-left").style.visibility = "visible";
                         }
                         else{
                             alert("Please choose another display name")
                         }
                     }
 
-                    //document.querySelector("display-name").style.visibility = "hidden"
-                    document.getElementById("display-name").style.visibility = "hidden"
-                    document.getElementById("nav-left").style.visibility = "visible"
                     // Data to send with request
                     const data = new FormData();
                     data.append('name', name.value);
 
                     // Send request
                     request.send(data);
-                    return false;
-
-                    
+                    return false; 
                 }
             }
             else{
@@ -61,9 +58,6 @@ document.addEventListener('DOMContentLoaded', () =>{
             }
         }   
     }
-
-
-
     //start by loading the index page 
 
 });
