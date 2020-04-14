74fb33323c9c710a3cbbe9822169fae359b31174
Habiba-Naeem <habiba.abdulnaeem@gmail.com>
2020-04-14 23:16:22 +0500

    alost done

--- application.py
+++ application.py
@@ -1,32 +1,53 @@
 import os
 
-from flask import Flask, redirect, render_template, request, url_for, jsonify
+from flask import Flask, redirect, render_template, request, url_for, jsonify, session
+from flask_session import Session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 socketio = SocketIO(app)
 
-display_name = []
+user_list = []
 
 @app.route("/")
 def index():
-    #return redirect(url_for("main"))
+
+    if session:
+        return redirect(url_for('main', username = session["username"]))
+
     return render_template("index.html")
+
+@app.route("/logout")
+def logout():
+
+    #forget user
+    session.clear()
+    return redirect(url_for('index'))
+
 
 @app.route("/sign", methods=["POST"])
 def sign():
 
-    name = request.form.get("name")
+    username = request.form.get("username")
     
-    if name not in display_name:
-        display_name.append(name)
-        print(*display_name, sep='\n')
+    if username not in user_list:
+        user_list.append(username)
+        session["username"] = username
+        print("Session: " , session["username"])
+        print(*user_list, sep='\n')
         return jsonify({"success":True} )
     
     else:
-        print(*display_name, sep='\n')
+        print(*user_list, sep='\n')
         return jsonify({"success":False})    
+
+@app.route("/user/<username>", methods=["GET", "POST"])
+def main(username):
+    return render_template("main.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
-    var okay_button = document.querySelector('#name');
-    var name = document.querySelector(".name");
-    var old_user_button =  document.querySelector('#old-user');
+    var okay_button = document.querySelector('#confirm-user');
+    var username = document.querySelector(".username");
+
+    //document.getElementById("nav-left").style.visibility = "hidden";
     // Connect to websocket
     //var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     
-
     // By default, submit button is disabled
     okay_button.disabled = true;
 
 
     // Enable button if there is some text in input field 
-    name.onkeyup = function(e){
+    username.onkeyup = function(e){
         // If spaces are given
         if ( e.which === 32 ){
             return false;
         }
         else{
-            if ( name.value.length > 0 ){
+            if ( username.value.length > 0 ){
                 okay_button.disabled = false;
     
                 // Now if button is clicked
@@ -33,20 +33,20 @@ document.addEventListener('DOMContentLoaded', () =>{
                         
                         // Extract JSON data from request
                         const data = JSON.parse(request.responseText);
-                        console.log(data);
+                    
                         if (data.success){
-                            localStorage.setItem("name", name.value);
-                            document.getElementById("display-name").style.visibility = "hidden";
-                            document.getElementById("nav-left").style.visibility = "visible";
+                            localStorage.setItem("username", username.value);
+                            window.location.replace(`/user/${username.value}`);
                         }
                         else{
-                            alert("Please choose another display name")
+                            localStorage.clear();
+                            alert("Please choose another display name");
                         }
                     }
 
                     // Data to send with request
                     const data = new FormData();
-                    data.append('name', name.value);
+                    data.append('username', username.value);
 
                     // Send request
                     request.send(data);
@@ -58,6 +58,5 @@ document.addEventListener('DOMContentLoaded', () =>{
             }
         }   
     }
-    //start by loading the index page 
-
+   
 });
