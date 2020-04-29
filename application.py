240802f397976b016e62f11e3de701a65833d6fa
Habiba-Naeem <habiba.abdulnaeem@gmail.com>
2020-04-29 22:25:49 +0500

    just need to check the software once more. Everything works fine

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
 import os
+import requests
+import unicodedata as ud
 
+from datetime import datetime
 from flask import Flask, redirect, render_template, request, url_for, jsonify, session
 from flask_session import Session
 from flask_socketio import SocketIO, emit
@@ -11,70 +14,163 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 socketio = SocketIO(app)
 
+#list of users and channels 
 user_list = []
 channel_list = []
+
+#dictionary to keep track of messages and users
+textdict = {}
+
 
 @app.route("/")
 def index():
 
-    if session:
+    #if user had joined a channel before, then display that channel
+    if "channel_name" in session:
+        return redirect(url_for('main_channel', username = session["username"], channel_name = session["channel_name"]))
+
+    #if user has newly joined the application, the display the main page
+    elif "username" in session:
         return redirect(url_for('main', username = session["username"]))
 
-    return render_template("index.html")
+    #user has not joined the application, then display the page where user can login
+    else:
+        return render_template("index.html")
 
 @app.route("/logout")
 def logout():
-
     #forget user
     session.clear()
     return redirect(url_for('index'))
 
-
-@app.route("/sign", methods=["POST", "GET"])
+#for AJAX request 
+@app.route("/sign", methods=["POST"])
 def sign():
+
+    #get username from html 
     username = request.form.get("username")
     
     if request.method == "POST":
+
+        #check if username does not exist in list 
         if username not in user_list:
+
+            #append and store in session
             user_list.append(username)
             session["username"] = username
+
             print("Session: " , session["username"])
             print(*user_list, sep='\n')
+
             return jsonify({"success":True})
-            #return redirect(url_for('main', username = username ))
+
+        #if username exists in the list
         else:
             print(*user_list, sep='\n')
             return jsonify({"success":False})
 
+
+#when user has provided a valid display name, then display the main.html
 @app.route("/user/<string:username>")
 def main(username):
     return render_template("main.html", username = username)
 
+#for AJAX request
 @app.route("/create-channel", methods=["POST"])
 def create_channel():
+
+    #get the channel name from html
     channel_name = request.form.get("channel-name")
+
     if request.method == "POST":
+
+        #check if channel name does not exist in the list
         if channel_name not in channel_list:
+
+            #append channel name and store in session
             channel_list.append(channel_name)
             session["channel_name"] = channel_name
+
             print("Session: " , session["channel_name"])
+            print("channel list:")
             print(*channel_list, sep='\n')
+
+            #make a key of the channel created in the textdict with empty array as its value
+            textdict[channel_name] = []
+            print("textdict:")
+            print(textdict)
+
             return jsonify({"success":True} )
         
         else:
             print(*channel_list, sep='\n')
             return jsonify({"success":False})
 
+
+#displaying the selected channel page
 @app.route("/user/<username>/<channel_name>", methods=["GET", "POST"])
 def main_channel(username, channel_name):
-    return render_template("main.html", channel_name = channel_name)
 
+    if request.method == "GET":
+
+        session["channel_name"] = channel_name
+       
+        print(f"Channel name:", session["channel_name"])
+        print(f"Userame:", session["username"])
+        print("channel list: ")
+        print(*channel_list)
+        print("textdict:")
+        print(textdict)
+
+        return render_template("main.html", channel_name = channel_name, username = username)
+    
+    #AJAX request to display previous messages. This will be generated from javascript file whenever a user goes to a channel
+    elif request.method == "POST":
+        return jsonify({'list_of_messages': textdict[ channel_list[ channel_list.index(session["channel_name" ] ) ] ]})
+        
+
+#for AJAX request 
 @app.route("/channel-list", methods = ["POST"])
 def channels():
+
+    #display the list of channels
+    print("channel list")
     print(*channel_list, sep='\n')
+
     return jsonify({
         "list":channel_list
     })
+
+
+@socketio.on("send message")
+def message(data):
+
+    #the text any user enters 
+    text_message = data["text_message"]
+    print("text_message:")
+    print(*text_message)
+    
+    #decode the emoji. Normal text will remain same, only the emojis will be decoded
+    emoji = text_message.encode('latin1').decode("utf8")
+    print(emoji)
+
+    #current date and time
+    time = datetime.now().strftime("%d-%m-%Y %H:%M")
+
+    #a list of messages 
+    list_of_messages = textdict[channel_list[ channel_list.index(session["channel_name" ] ) ]]
+
+    list_of_messages.append({'text_message': emoji, 'username':session["username"], 'channel_name': session["channel_name"], 'time': time})
+    print(list_of_messages)  
+    
+    #if length of messages reaches 100
+    if len(list_of_messages) == 100: 
+        del list_of_messages[0]
+
+    text_list = {'text_message': emoji, 'username':session["username"], 'channel_name': session["channel_name"], 'time': time}
+    
+    emit("message", text_list, broadcast=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,17 +1,23 @@
+// add_list_items() will create the list of channel when "Channel List" button is clicked
 function add_list_items(data, list_div, ul){
 
+    // When the div is not displayed
     if ( list_div.style.display === "none" ){
 
-        while (ul.firstChild) {
-            ul.removeChild(ul.firstChild);
-        }
+        // Remove the list items
+        while ( ul.firstChild ) {
+            ul.removeChild( ul.firstChild );
+        };
 
+        // Add channels to the list
         for ( let i = 0; i < data["list"].length; i++ ){
+
             const li = document.createElement('li');
             const a = document.createElement('a');
 
+            // text contains the name of the channel
             var text = document.createTextNode(data["list"][i]);
-            
+           
             a.href = `/user/${localStorage.getItem("username")}/${data["list"][i]}`;
             a.appendChild(text);
 
@@ -19,9 +25,18 @@ function add_list_items(data, list_div, ul){
             li.appendChild(a);
     
             ul.append(li);
+
+            // when the channel name is clicked 
+            a.addEventListener("click", () => {
+                localStorage.setItem("channel-name", a.innerHTML);
+                console.log(localStorage.getItem("channel-name"));
+            })
         }
+
+        // to display the div
         list_div.style.display = "block";
     }
+    // If the div was already displayed, then set it to none
     else{
         list_div.style.display = "none";
     }
@@ -30,19 +45,44 @@ function add_list_items(data, list_div, ul){
 
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    // buttons:
     var okay_button = document.querySelector("#confirm-user");
+    var logout = document.querySelector("#logout");
+    var create_channel_button = document.querySelector("#create-channel-button");
+    var create = document.querySelector("#create");
+    var emoji = document.querySelector("#emoji");
+    var channel_list_button = document.querySelector("#channel-list");
+
+    // input field
     var username = document.querySelector(".username");
 
-    var create_channel_button = document.querySelector("#create-channel-button");
+    // div
     var create_channel_div = document.querySelector("#create-channel-div");
-    var create = document.querySelector("#create");
-    var channel_list_button = document.querySelector("#channel-list");
     var channel_list_div = document.querySelector("#channel-list-div");
-    var channel_list_ul = document.querySelector('#channel-list-ul');
-    //document.querySelector(".nav-left").style.visibility = "hidden";
     
+
+    // unordered list 
+    var channel_list_ul = document.querySelector('#channel-list-ul');
+    
+    // invisible side bar
+    document.querySelector(".nav-left").style.visibility = "hidden";
+    
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    
+
+    // if user has logged in then show the side bar
+    if(localStorage.getItem("username")){
+        document.querySelector(".nav-left").style.visibility = "visible";
+    }
+
+    // user confirms the display name
     if(okay_button){
+
         okay_button.addEventListener("click", () => {
+            
             // Initialise a new request
             const request = new XMLHttpRequest();
             request.open('POST', '/sign');
@@ -69,6 +109,15 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     };
 
+    // if user logout then clear the localStorage
+    if(logout){
+        logout.addEventListener("click", () =>{
+            localStorage.clear();
+            window.location.replace('/logout');
+        })
+    };
+
+    // to display the div where user can create a channel
     if(create_channel_button){
         create_channel_button.addEventListener("click", () => {
             if ( create_channel_div.style.display === "none" ){
@@ -80,8 +129,31 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
+    // to display the list of channels
+    if (channel_list_button){
+
+        channel_list_button.addEventListener("click", () =>{
+            
+            // Initialise a new request
+            const request = new XMLHttpRequest();
+            request.open('POST', '/channel-list');
+            
+            request.onload = () =>{
+                // Extract JSON data from request
+                const data = JSON.parse(request.responseText);   
+
+                // sending the json response from server, the div to display channel list, and the unordered list
+                add_list_items(data, channel_list_div, channel_list_ul);
+            }
+            request.send();
+            return false;
+        })
+    }
+
+    // when user creates a channel
     if(create){
         create.addEventListener("click", () => {
+
             // Initialise a new request
             const request = new XMLHttpRequest();
             request.open('POST', '/create-channel');
@@ -108,24 +180,138 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     };
 
-    if (localStorage.getItem("channel-name")){
-        document.querySelector("#text").style.display = "inline-block";
-    }
-    
-    if (channel_list_button){
+    // add emoji to the text
+    if (emoji){
 
-        channel_list_button.addEventListener("click", () =>{
-            // Initialise a new request
-            const request = new XMLHttpRequest();
-            request.open('POST', '/channel-list');
-            
-            request.onload = () =>{
-                // Extract JSON data from request
-                const data = JSON.parse(request.responseText);   
-                add_list_items(data, channel_list_div, channel_list_ul);
+        var div = document.querySelector("#emoji-div");
+
+        // to create 16 emojis
+        for ( let i = 0; i < 16; i++){
+
+            var emo1 = document.createElement("button");
+            emo1.id = "emos";
+
+            if( i < 10){
+                emo1.innerHTML = String.fromCodePoint('0x1F60'+i);
+                console.log(emo1);
+                div.appendChild(emo1);
             }
-            request.send();
-            return false;
-        })
+            // change i to hexadecimal
+            else{
+                hexString = i.toString(16).toUpperCase();
+                emo1.innerHTML = String.fromCodePoint('0x1F60'+ hexString);
+                console.log(emo1);
+                div.appendChild(emo1);
+            }
+        }
+        // when the smiley face emoji is clicked
+        emoji.addEventListener("click", () => {
+            if ( div.style.display === "none" ){
+                div.style.display = "inline-block";
+            }
+            else{
+                div.style.display = "none";
+            }
+            document.querySelector("#text").append(div);
+        });
+
+        // when user selects from the group of emojis
+        var emos = document.querySelectorAll("#emos");
+        if (emos){
+            emos.forEach( emos => {
+                emos.addEventListener("click", () =>{
+                    document.querySelector("#message").value += emos.innerHTML;
+                });
+            })
+        };
+    };
+
+    // previous 100 messages to be displayed
+    if (localStorage.getItem("channel-name")){
+        // display the fieldset
+        document.querySelector("#text").style.display = "inline-block";
+
+        // Initialise a new request
+        const request = new XMLHttpRequest();
+        request.open('POST', `/user/${localStorage.getItem("username")}/${localStorage.getItem("channel-name")}`);
+        
+        request.onload = () => {
+            // Extract JSON data from request
+            const data = JSON.parse(request.responseText);
+            console.log(data);
+            for (let i = 0; i < data["list_of_messages"].length ; i++){
+
+                const p = document.createElement('p');
+                const span1 = document.createElement('span');
+                const span2 = document.createElement('span');
+
+
+                var text = document.createTextNode(data["list_of_messages"][i]["text_message"]);
+                console.log("existsit text :", text);
+                p.appendChild(text);
+                
+                        
+                var user =  document.createTextNode(data["list_of_messages"][i]["username"]);
+                console.log(user);
+                span1.appendChild(user);
+
+                var time =  document.createTextNode(data["list_of_messages"][i]["time"]);
+                console.log(time);
+                span2.appendChild(time);
+
+
+                const div = document.createElement('div');
+                div.className = "messages-div";
+
+                div.appendChild(p);
+                div.appendChild(span1);
+                div.appendChild(span2);
+                document.querySelector(".all-messages").append(div);
+                // bring the scroll bar to bottom
+                document.querySelector(".all-messages").scrollTop = document.querySelector(".all-messages").scrollHeight;
+
+            }
+        }
+        request.send();
     }
+
+    // When connected, configure buttons
+    socket.on('connect', () => {
+        document.querySelector('#send-message').addEventListener("click", () => {
+            const text_message = document.querySelector("#message").value;
+
+            socket.emit('send message', {'text_message':text_message});
+        });
+    });
+
+    // When a new message is announced, add to the unordered list
+    socket.on('message', function(data){
+        if (localStorage.getItem("channel-name") === data["channel_name"]){
+            const p = document.createElement('p');
+            const span1 = document.createElement('span');
+            const span2 = document.createElement('span');
+
+            var text = document.createTextNode(data["text_message"]);
+            console.log("text:", text);
+            p.appendChild(text);
+            
+            var user =  document.createTextNode(data["username"]);
+            console.log(user);
+            span1.appendChild(user);
+
+            var time =  document.createTextNode(data["time"]);
+            console.log(time);
+            span2.appendChild(time);
+
+
+            const div = document.createElement('div');
+            div.className = "messages-div";
+
+            div.appendChild(p);
+            div.appendChild(span1);
+            div.appendChild(span2);
+            document.querySelector(".all-messages").append(div);
+        }
+    });
+
 });
